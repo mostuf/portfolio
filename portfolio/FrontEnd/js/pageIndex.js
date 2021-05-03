a663c627187e001c7a10448bcb4f7eb767a210bb
Chaplin16 <chaplin16@orange.fr>
2021-05-04 00:01:53 +0200

    style premiere deuxieme et troisieme page ok

---
+++ portfolio/FrontEnd/js/pageIndex.js
@@ -0,0 +1,35 @@
+let btnSubmitMessage = document.getElementById("btnSubmitMessage");
+
+btnSubmitMessage.addEventListener("click", function (event) { // envoie du formulaire au click du bouton
+    let formContact = document.getElementById("formContact");
+    event.preventDefault();
+
+    if( formContact.reportValidity() == true) {//verification si le formulaire est correctement rempli
+        let contact = { // je cree un objet avec les valeurs que je recupere par les id
+            'username': document.getElementById("username").value,
+            'email': document.getElementById("email").value,
+            'message': document.getElementById("message").value
+        }     
+        let sendInfo = JSON.stringify( contact );
+        //j'envoie des données au serveur    
+        fetch('#' +'/formContact', {
+            method: "post",
+            headers: {"Content-Type": "application/json;charset=UTF-8"},
+            mode:"cors",
+            body: sendInfo            
+        })
+        .then(function (response) {
+            return response.json()
+        }) 
+        .then(function() { //j enregistre le retour  de l'api dans des variables
+            
+//ouverture de la page de confirmation ave les parametres dans l url
+               window.location.assign("index.html")
+        })
+        //le retour en cas de non connection au serveur 
+        .catch(function(err) {
+        console.log('Retour info Api problem: ' + err.message);
+        })
+}
+
+});
