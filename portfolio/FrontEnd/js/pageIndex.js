0778338b775fc6f8a2f5c03778057c9fbf0eb25d
Chaplin16 <chaplin16@orange.fr>
2021-05-10 13:55:10 +0200

    initialisation avec emailjs

--- portfolio/FrontEnd/js/pageIndex.js
+++ portfolio/FrontEnd/js/pageIndex.js
@@ -1,41 +1,53 @@
 let btnSubmitMessage = document.getElementById("btnSubmitMessage");
 
-btnSubmitMessage.addEventListener("click", function (event) { // envoie du formulaire au click du bouton
+btnSubmitMessage.addEventListener('click', function (event) { // envoie du formulaire au click du bouton
     let formContact = document.getElementById("formContact");
     event.preventDefault();
 
     if( formContact.reportValidity() == true) {//verification si le formulaire est correctement rempli
         const regexContent = /[^><%#!$/]{15,400}$/;
         if(!regexContent.test(document.getElementById("message").value)){
-            alert( "Votre message doit contenir entre 15 et 400 caractères sans caractère speciaux. Merci d'effectuer les modifications nécessaires. Aurore")
+            alert( "Votre message doit contenir entre 15 et 400 caractères sans caractère spéciaux. Merci d'effectuer les modifications nécessaires. Aurore")
             return false
         }
         let contact = { // je cree un objet avec les valeurs que je recupere par les id
             'username': document.getElementById("username").value,
             'email': document.getElementById("email").value,
-            'message': document.getElementById("message").value
+            'message': document.getElementById("message").value,
+            'phone': document.getElementById("phone").value
         }     
-        let sendInfo = JSON.stringify( contact );
+        
+        emailjs.sendForm('my_gmail', contact, this)
+        .then(function() {
+            console.log('SUCCESS!');
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+
+
+
+
         //j'envoie des données au serveur    
-        fetch('http://localhost:3000' +'/form', {
-            method: "post",
-            headers: {"Content-Type": "application/json;charset=UTF-8"},
-            mode:"cors",
-            body: sendInfo            
-        })
-        .then(function (response) {
-            return response.json()
-        }) 
-        .then(function() { //j enregistre le retour  de l'api dans des variables
-            alert( 'Votre message a bien été expédié! Merci')
-            document.getElementById("username").value = "";
-            document.getElementById("email").value= "";
-            document.getElementById("message").value= "";
-        })
-        //le retour en cas de non connection au serveur 
-        .catch(function(err) {
-        console.log('Retour info Api problem: ' + err.message);
-        })
+        // fetch('http://localhost:3000' +'/form', {
+        //     method: "post",
+        //     headers: {"Content-Type": "application/json;charset=UTF-8"},
+        //     mode:"cors",
+        //     body: sendInfo            
+        // })
+        // .then(function (response) {
+        //     return response.json()
+        // }) 
+        // .then(function() { //j enregistre le retour  de l'api dans des variables
+        //     alert( 'Votre message a bien été expédié! Merci')
+        //     document.getElementById("username").value = "";
+        //     document.getElementById("email").value= "";
+        //     document.getElementById("message").value= "";
+        //     document.getElementById("phone").value= "";
+        // })
+        // //le retour en cas de non connection au serveur 
+        // .catch(function(err) {
+        // console.log('Retour info Api problem: ' + err.message);
+        // })
     }
 
 });
